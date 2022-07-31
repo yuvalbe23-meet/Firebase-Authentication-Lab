90f7ba2da0d68d3f2138a76ffb6ef391ff478756
yuvalbe23-meet <109899974+yuvalbe23-meet@users.noreply.github.com>
2022-07-31 14:35:04 +0300

    Lab finished

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
@@ -6,13 +7,46 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
+config = {
+  "apiKey": "AIzaSyA6bm8pnGzHj6pBaN-7mLQpnOAxPEGgpTI",
+  "authDomain": "test-882ae.firebaseapp.com",
+  "projectId": "test-882ae",
+  "storageBucket": "test-882ae.appspot.com",
+  "messagingSenderId": "559871725242",
+  "appId": "1:559871725242:web:c1339d21b4f0f739e1bc83",
+  "measurementId": "G-NX9GJB39PJ",
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth= firebase.auth()
+
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    eror=""
+    if request.method == 'POST':
+        email= request.form['email']
+        password= request.form['password']
+        try: 
+            login_session['user']= auth.sign_in_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+        except:
+            eror="Not Working"
     return render_template("signin.html")
+   
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    eror=""
+    if request.method == 'POST':
+        email= request.form['email']
+        password= request.form['password']
+        try: 
+            login_session['user']= auth.create_user_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+        except:
+            eror="Not Working"
     return render_template("signup.html")
 
 

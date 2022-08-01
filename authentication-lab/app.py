721b4e5ccdfc60590939e7c363284d4d7ecf6c08
yuvalbe23-meet <109899974+yuvalbe23-meet@users.noreply.github.com>
2022-08-01 10:17:00 +0300

    2 LABS WITH + BONUS OF FIRST

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -15,11 +15,13 @@ config = {
   "messagingSenderId": "559871725242",
   "appId": "1:559871725242:web:c1339d21b4f0f739e1bc83",
   "measurementId": "G-NX9GJB39PJ",
-  "databaseURL": ""
+  "databaseURL": "https://test-882ae-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(config)
 auth= firebase.auth()
+db= firebase.database()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
@@ -27,6 +29,7 @@ def signin():
     if request.method == 'POST':
         email= request.form['email']
         password= request.form['password']
+
         try: 
             login_session['user']= auth.sign_in_with_email_and_password(email,password)
             return redirect(url_for('add_tweet'))
@@ -39,21 +42,41 @@ def signin():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     eror=""
+
     if request.method == 'POST':
         email= request.form['email']
         password= request.form['password']
+
+        user={"email": request.form['email'],"password": request.form['password'],"full_name": request.form['full_name'],"username": request.form['username'] }
         try: 
             login_session['user']= auth.create_user_with_email_and_password(email,password)
+            db.child("Users").child(login_session['user']['localId']).set(user)
             return redirect(url_for('add_tweet'))
         except:
             eror="Not Working"
     return render_template("signup.html")
 
 
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
+
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    eror=""
+    if request.method== 'POST':
+        tweet={"title": request.form['title'],"text":request.form['text'],"uid":login_session['user']['localId']}
+        try:
+            db.child("Users").child(login_session['user']['localId']).child("Tweets").push(tweet)
+            return redirect(url_for('all_tweets'))
+        except:
+            eror="Eror"
     return render_template("add_tweet.html")
 
-
+@app.route('/all_tweets')
+def all_tweets():
+    return render_template("all_tweets.html",all_tweets= db.child("Users").child(login_session['user']['localId']).child("Tweets").get().val())
 if __name__ == '__main__':
     app.run(debug=True)
